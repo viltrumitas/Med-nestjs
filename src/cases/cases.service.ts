36e2a2412788488535ef815e20575f4c22f65375
Ismael <ismaelazcatl055@gmail.com>
2026-06-02 15:43:06 -0600

    Antes, solo se creaba un case con status por defecto en draft, ahora se implemento el metodo submit, esto una vez enviado modifica el status del case actual como submit. Tambien tiene validaciones que son estas: Caso no encontrado, no puedes mandar casos que te pertenecen y no se puede volver a enviar un caso

--- src/cases/cases.service.ts
+++ src/cases/cases.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { caseWithAuthor } from '../common/utils/safe-user-select';
 import { CaseMapper } from './mappers/case.mapper';
+import { CaseStatus } from '@prisma/client';
 
 @Injectable()
 export class CasesService {
@@ -22,6 +28,31 @@ export class CasesService {
     });
 
     return CaseMapper.toResponse(createdCase);
+  }
+
+  async submit(id: string, userId: string) {
+    const caseEntity = await this.prisma.case.findUnique({
+      where: { id },
+    });
+
+    if (!caseEntity) {
+      throw new NotFoundException('Case not found');
+    }
+
+    if (caseEntity.authorId !== userId) {
+      throw new ForbiddenException('You can only submit your own cases');
+    }
+
+    if (caseEntity.status !== CaseStatus.DRAFT) {
+      throw new BadRequestException('Only draft cases can be submitted');
+    }
+
+    return this.prisma.case.update({
+      where: { id },
+      data: {
+        status: CaseStatus.SUBMITTED,
+      },
+    });
   }
 
   async findAll() {
