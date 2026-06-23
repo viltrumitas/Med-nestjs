118910ce88f4d6a5d9517326569a0c098ac471a6
Ismael Azcatl <ismaelazcatl055@gmail.com>
2026-06-22 22:24:21 -0600

    deploy backend inicial

--- src/cases/dto/teacher-response.dto.ts
+++ src/cases/dto/teacher-response.dto.ts
@@ -1,35 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '@prisma/client';
 
 export class TeacherResponseDto {
-  @ApiProperty({
-    description: 'ID único del autor',
-    example: '550e8400-e29b-41d4-a716-446655440000',
-  })
   id!: string;
-
-  @ApiProperty({
-    description: 'Matricula del autor',
-    example: 202200008,
-  })
   matricula!: number;
-
-  @ApiProperty({
-    description: 'Primer nombre del autor',
-    example: 'Juan',
-  })
   firstName!: string;
-
-  @ApiProperty({
-    description: 'Apellido del autor',
-    example: 'García',
-  })
   lastName!: string;
-
-  @ApiProperty({
-    description: 'Rol del usuario',
-    enum: UserRole,
-    example: 'STUDENT',
-  })
   role!: UserRole;
 }
--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,13 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:4200/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  
   const config = new DocumentBuilder()
     .setTitle('Med NestJS - Clinical Cases API')
     .setDescription(
